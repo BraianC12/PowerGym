401adb8f1de6d5ea1ac322b79a57a9c40a6304d8
JesicaBelenAlfonso <jesicaalfonso2005@gmail.com>
2026-09-22 19:28:24 -0300

    front de administrador

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const { sequelize } = require('./models');
 const socioRoutes = require('./routes/socioRoutes');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware para que Express entienda JSON
 app.use(express.json());
-
+//sin los headers CORS la petición se rechaza. app.use(cors()) 
+app.use(cors());
 // Rutas de la API
 app.use('/api/socios', socioRoutes);
 
