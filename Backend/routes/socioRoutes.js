0f0c296437479a732492cf9b12de727044f5de8d
Braian Carranza <braianenrique89@gmail.com>
2026-09-26 20:07:32 -0300

    feat: agregar endpoint PUT para modificar datos de socios

--- Backend/routes/socioRoutes.js
+++ Backend/routes/socioRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { crearSocio, darDeBajaSocio, obtenerSocios } = require('../controllers/socioController');
-
+const { editarSocio } = require('../controllers/socioController')
 
 router.post('/', crearSocio); 
 router.get('/', obtenerSocios); 
-router.patch('/:id', darDeBajaSocio); 
+router.patch('/:id', darDeBajaSocio);
+router.put('/:id', editarSocio);
+ 
 
 
 module.exports = router;
